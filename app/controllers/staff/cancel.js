1eddca61d46d6f78f2515b2677c070383eff8518
Kusunose <kusunose@motionpicture.jp>
2017-08-02 14:33:14 +0900

    TTTS-84 スタッフ・予約キャンセル実装(中)

--- app/controllers/staff/cancel.js
+++ app/controllers/staff/cancel.js
@@ -24,6 +24,8 @@ function execute(req, res, next) {
             return;
         }
         const staffUser = req.staffUser;
+        const successIds = [];
+        const errorIds = [];
         try {
             // 予約IDリストをjson形式で受け取る
             const reservationIds = JSON.parse(req.body.reservationIds);
@@ -31,22 +33,107 @@ function execute(req, res, next) {
                 throw new Error(req.__('Message.UnexpectedError'));
             }
             const promises = reservationIds.map((id) => __awaiter(this, void 0, void 0, function* () {
-                debug('updating to STATUS_KEPT_BY_TTTS by staff... staff:', staffUser.get('user_id'), 'signature:', staffUser.get('signature'), 'id:', id);
-                const reservation = yield ttts_domain_1.Models.Reservation.findOneAndUpdate({ _id: id }, { status: ttts_domain_2.ReservationUtil.STATUS_KEPT_BY_TTTS }, { new: true }).exec();
-                debug('updated to STATUS_KEPT_BY_TTTS by staff.', reservation, 'staff:', staffUser.get('user_id'), 'signature:', staffUser.get('signature'), 'id:', id);
+                debug('updating to AVAILABLE by staff... staff:', staffUser.get('user_id'), 'signature:', staffUser.get('signature'), 'id:', id);
+                // const reservation = await Models.Reservation.findOneAndUpdate(
+                //     { _id: id },
+                //     { status: ReservationUtil.STATUS_KEPT_BY_TTTS },
+                //     { new: true }
+                // ).exec();
+                const result = yield cancelById(id);
+                // debug(
+                //     'updated to STATUS_KEPT_BY_TTTS by staff.', reservation,
+                //     'staff:', staffUser.get('user_id'),
+                //     'signature:', staffUser.get('signature'),
+                //     'id:', id
+                // );
+                if (result) {
+                    successIds.push(id);
+                }
+                else {
+                    errorIds.push(id);
+                }
             }));
             yield Promise.all(promises);
             res.json({
                 success: true,
-                message: null
+                message: null,
+                successIds: successIds,
+                errorIds: errorIds
             });
         }
         catch (error) {
             res.json({
                 success: false,
-                message: error.message
+                message: error.message,
+                successIds: successIds,
+                errorIds: errorIds
             });
         }
     });
 }
 exports.execute = execute;
+/**
+ * キャンセル処理(idから)
+ *
+ * @param {string} reservationId
+ * @return {Promise<boolean>}
+ */
+function cancelById(reservationId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // idから予約データ取得
+            const reservation = yield ttts_domain_1.Models.Reservation.findById(reservationId).exec();
+            // キャンセルメール送信
+            //await sendEmail(reservations[0].purchaser_email, getCancelMail(reservations));
+            //logger.info('-----update db start-----');
+            // 予約データ解放(AVAILABLEに変更)
+            yield ttts_domain_1.Models.Reservation.findByIdAndUpdate(reservation._id, {
+                $set: { status: ttts_domain_2.ReservationUtil.STATUS_AVAILABLE },
+                $unset: getUnsetFields(reservation._doc)
+            }).exec();
+            //logger.info('Reservation clear =', JSON.stringify(reservation));
+            const tickets = ttts_domain_1.Models.CustomerCancelRequest.getTickets([reservation]);
+            // キャンセルリクエスト保管
+            yield ttts_domain_1.Models.CustomerCancelRequest.create({
+                reservation: reservation,
+                //tickets: (<any>Models.CustomerCancelRequest).getTickets([reservation]),
+                tickets: tickets,
+                cancel_name: reservation.owner_name.ja,
+                cancellation_fee: 0
+            });
+            // logger.info('CustomerCancelRequest create =', JSON.stringify(reservations[0]));
+            // logger.info('-----update db end-----');
+        }
+        catch (error) {
+            return false;
+        }
+        return true;
+    });
+}
+/**
+ * 更新時削除フィールド取得
+ *
+ * @param {any} reservation
+ * @return {any} unset
+ */
+function getUnsetFields(reservation) {
+    const setFields = [
+        '_id',
+        'performance',
+        'seat_code',
+        'updated_at',
+        'checkins',
+        'performance_canceled',
+        'status',
+        '__v',
+        'created_at'
+    ];
+    const unset = {};
+    // セットフィールド以外は削除フィールドにセット
+    Object.getOwnPropertyNames(reservation).forEach((propertyName) => {
+        if (setFields.indexOf(propertyName) < 0) {
+            unset[propertyName] = 1;
+        }
+    });
+    return unset;
+}
--- app/controllers/staff/cancel.ts
+++ app/controllers/staff/cancel.ts
@@ -17,7 +17,8 @@ export async function execute(req: Request, res: Response, next: NextFunction): 
         return;
     }
     const staffUser = req.staffUser;
-
+    const successIds: string[] = [];
+    const errorIds: string[] = [];
     try {
         // 予約IDリストをjson形式で受け取る
         const reservationIds = JSON.parse(req.body.reservationIds);
@@ -27,33 +28,110 @@ export async function execute(req: Request, res: Response, next: NextFunction): 
 
         const promises = reservationIds.map(async (id) => {
             debug(
-                'updating to STATUS_KEPT_BY_TTTS by staff... staff:', staffUser.get('user_id'),
+                'updating to AVAILABLE by staff... staff:', staffUser.get('user_id'),
                 'signature:', staffUser.get('signature'),
                 'id:', id
             );
-            const reservation = await Models.Reservation.findOneAndUpdate(
-                { _id: id },
-                { status: ReservationUtil.STATUS_KEPT_BY_TTTS },
-                { new: true }
-            ).exec();
-            debug(
-                'updated to STATUS_KEPT_BY_TTTS by staff.', reservation,
-                'staff:', staffUser.get('user_id'),
-                'signature:', staffUser.get('signature'),
-                'id:', id
-            );
+            // const reservation = await Models.Reservation.findOneAndUpdate(
+            //     { _id: id },
+            //     { status: ReservationUtil.STATUS_KEPT_BY_TTTS },
+            //     { new: true }
+            // ).exec();
+            const result: boolean = await cancelById(id);
+            // debug(
+            //     'updated to STATUS_KEPT_BY_TTTS by staff.', reservation,
+            //     'staff:', staffUser.get('user_id'),
+            //     'signature:', staffUser.get('signature'),
+            //     'id:', id
+            // );
+            if (result) {
+                successIds.push(id);
+            } else {
+                errorIds.push(id);
+            }
         });
-
         await Promise.all(promises);
-
         res.json({
             success: true,
-            message: null
+            message: null,
+            successIds: successIds,
+            errorIds: errorIds
         });
     } catch (error) {
         res.json({
             success: false,
-            message: error.message
+            message: error.message,
+            successIds: successIds,
+            errorIds: errorIds
         });
     }
+}
+/**
+ * キャンセル処理(idから)
+ *
+ * @param {string} reservationId
+ * @return {Promise<boolean>}
+ */
+async function cancelById(reservationId: string) : Promise<boolean> {
+    try {
+        // idから予約データ取得
+        const reservation: any = await Models.Reservation.findById(reservationId).exec();
+        // キャンセルメール送信
+        //await sendEmail(reservations[0].purchaser_email, getCancelMail(reservations));
+        //logger.info('-----update db start-----');
+        // 予約データ解放(AVAILABLEに変更)
+        await Models.Reservation.findByIdAndUpdate(
+            reservation._id,
+            {
+                $set: { status: ReservationUtil.STATUS_AVAILABLE },
+                $unset: getUnsetFields(reservation._doc)
+            }
+        ).exec();
+        //logger.info('Reservation clear =', JSON.stringify(reservation));
+
+        const tickets = (<any>Models.CustomerCancelRequest).getTickets([reservation]);
+        // キャンセルリクエスト保管
+        await Models.CustomerCancelRequest.create({
+            reservation: reservation,
+            //tickets: (<any>Models.CustomerCancelRequest).getTickets([reservation]),
+            tickets: tickets,
+            cancel_name: reservation.owner_name.ja,
+            cancellation_fee: 0
+        });
+        // logger.info('CustomerCancelRequest create =', JSON.stringify(reservations[0]));
+        // logger.info('-----update db end-----');
+    } catch (error) {
+
+        return false;
+    }
+
+    return true;
+}
+/**
+ * 更新時削除フィールド取得
+ *
+ * @param {any} reservation
+ * @return {any} unset
+ */
+function getUnsetFields(reservation: any): any {
+    const setFields: string[] = [
+        '_id',
+        'performance',
+        'seat_code',
+        'updated_at',
+        'checkins',
+        'performance_canceled',
+        'status',
+        '__v',
+        'created_at'
+    ];
+    const unset = {};
+    // セットフィールド以外は削除フィールドにセット
+    Object.getOwnPropertyNames(reservation).forEach((propertyName) => {
+        if (setFields.indexOf(propertyName) < 0) {
+            (<any>unset)[propertyName] = 1;
+        }
+    });
+
+    return unset;
 }
