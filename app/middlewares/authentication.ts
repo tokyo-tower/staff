ff39e975db2046ec2cbc173debb5f46b31192eaa
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-01-06 03:22:41 +0000

    # TTTS-208 ### Added - 管理者サービスを追加。

--- app/middlewares/authentication.ts
+++ app/middlewares/authentication.ts
@@ -4,12 +4,10 @@
  */
 
 import * as tttsapi from '@motionpicture/ttts-api-nodejs-client';
-import * as ttts from '@motionpicture/ttts-domain';
 import { NextFunction, Request, Response } from 'express';
 
 import StaffUser from '../models/user/staff';
 
-// tslint:disable-next-line:max-func-body-length
 export default async (req: Request, res: Response, next: NextFunction) => {
     req.staffUser = StaffUser.PARSE(req.session);
 
@@ -34,11 +32,11 @@ export default async (req: Request, res: Response, next: NextFunction) => {
             state: ''
         });
         oauth2Client.setCredentials({
-            refresh_token: cognitoCredentials.RefreshToken,
+            refresh_token: cognitoCredentials.refreshToken,
             // expiry_date: moment().add(<number>authenticationResult.ExpiresIn, 'seconds').unix(),
             // expiry_date: authenticationResult.ExpiresIn,
-            access_token: cognitoCredentials.AccessToken,
-            token_type: cognitoCredentials.TokenType
+            access_token: cognitoCredentials.accessToken,
+            token_type: cognitoCredentials.tokenType
         });
         req.tttsAuthClient = oauth2Client;
 
@@ -48,42 +46,42 @@ export default async (req: Request, res: Response, next: NextFunction) => {
     }
 
     // 自動ログインチェック
-    if (req.cookies.remember_staff !== undefined) {
-        try {
-            const authenticationDoc = await ttts.Models.Authentication.findOne(
-                {
-                    token: req.cookies.remember_staff,
-                    owner: { $ne: null }
-                }
-            ).exec();
+    // if (req.cookies.remember_staff !== undefined) {
+    //     try {
+    //         const authenticationDoc = await ttts.Models.Authentication.findOne(
+    //             {
+    //                 token: req.cookies.remember_staff,
+    //                 owner: { $ne: null }
+    //             }
+    //         ).exec();
 
-            if (authenticationDoc === null) {
-                res.clearCookie('remember_staff');
-            } else {
-                // トークン再生成
-                const token = ttts.CommonUtil.createToken();
-                await authenticationDoc.update({ token: token }).exec();
+    //         if (authenticationDoc === null) {
+    //             res.clearCookie('remember_staff');
+    //         } else {
+    //             // トークン再生成
+    //             const token = ttts.CommonUtil.createToken();
+    //             await authenticationDoc.update({ token: token }).exec();
 
-                // tslint:disable-next-line:no-cookies
-                res.cookie('remember_staff', token, { path: '/', httpOnly: true, maxAge: 604800000 });
-                const ownerRepo = new ttts.repository.Owner(ttts.mongoose.connection);
-                const owner = await ownerRepo.ownerModel.findOne({ _id: authenticationDoc.get('owner') }).exec();
-                if (owner === null) {
-                    throw new Error(res.__('UnexpectedError'));
-                }
+    //             // tslint:disable-next-line:no-cookies
+    //             res.cookie('remember_staff', token, { path: '/', httpOnly: true, maxAge: 604800000 });
+    //             const ownerRepo = new ttts.repository.Owner(ttts.mongoose.connection);
+    //             const owner = await ownerRepo.ownerModel.findOne({ _id: authenticationDoc.get('owner') }).exec();
+    //             if (owner === null) {
+    //                 throw new Error(res.__('UnexpectedError'));
+    //             }
 
-                // ログインしてリダイレクト
-                (<Express.Session>req.session).staffUser = <any>owner.toObject();
-                res.redirect(req.originalUrl);
+    //             // ログインしてリダイレクト
+    //             (<Express.Session>req.session).staffUser = <any>owner.toObject();
+    //             res.redirect(req.originalUrl);
 
-                return;
-            }
-        } catch (error) {
-            next(error);
+    //             return;
+    //         }
+    //     } catch (error) {
+    //         next(error);
 
-            return;
-        }
-    }
+    //         return;
+    //     }
+    // }
 
     if (req.xhr) {
         res.json({
